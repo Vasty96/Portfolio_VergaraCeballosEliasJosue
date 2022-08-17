dff5d96d721c9fa2b08c9a6bb51720f7dacfd499
Vasty96 <101201688+Vasty96@users.noreply.github.com>
2022-08-17 18:28:33 -0300

    Creacion de BACKEND con SpringBoot

---
+++ Portfolio_VergaraCeballosEliasJosue/BACKEND/src/test/java/com/myportfolio/ejvc/EjvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolio.ejvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
