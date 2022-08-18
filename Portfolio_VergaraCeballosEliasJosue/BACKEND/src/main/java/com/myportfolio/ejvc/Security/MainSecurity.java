a7c3aafd2ca2ce0107e836c0519a296b6126f6c5
Vasty96 <101201688+Vasty96@users.noreply.github.com>
2022-08-18 18:23:31 -0300

    Update MainSecurity.java

--- Portfolio_VergaraCeballosEliasJosue/BACKEND/src/main/java/com/myportfolio/ejvc/Security/MainSecurity.java
+++ Portfolio_VergaraCeballosEliasJosue/BACKEND/src/main/java/com/myportfolio/ejvc/Security/MainSecurity.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author VastyPC
@@ -48,6 +49,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
