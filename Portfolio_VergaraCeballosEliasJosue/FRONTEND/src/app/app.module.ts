4afd077372f55343eee4248d9646b9b43c06f083
Vasty96 <101201688+Vasty96@users.noreply.github.com>
2022-08-19 13:35:21 -0300

    Implementado Login

--- Portfolio_VergaraCeballosEliasJosue/FRONTEND/src/app/app.module.ts
+++ Portfolio_VergaraCeballosEliasJosue/FRONTEND/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { NavComponent } from './components/nav/nav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -34,11 +36,14 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
